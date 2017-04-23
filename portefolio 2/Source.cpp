a500e76442e4a9af4dbbf31751e0759ef1cf571d
AndersEllinge <andersellinge.ae@gmail.com>
2017-04-23 22:16:25 +0200

    Cleaned code, it output only relevant information

--- portefolio 2/Source.cpp
+++ portefolio 2/Source.cpp
@@ -84,7 +84,6 @@ Doub f(Doub gamma) {
 
 Doub integralFunc(Doub t) {
 	Doub bigGamma = pow((pow(1 * t, 2) / 0.0025) + (pow(Y, 2) / 0.04) + 1, -((Doub)1 / (Doub)2));
-	//std::cout << "bigGamma: " << bigGamma << std::endl;
 
 	return 0.744 * pow(bigGamma, 3) * f(bigGamma);
 }
@@ -93,43 +92,47 @@ Doub cTrap(Doub inputY, int N) {
 	if (abs(inputY) > (Doub)0.2)
 		return (Doub)0.0;
 
-	//std::cout << "I made it past catch" << std::endl;
+	Y = inputY;
+
+	Doub alpha = (Doub)0.05 * sqrt((Doub)1 - (pow(inputY, (Doub)2) / (Doub)0.04));
+
+	Doub res = trapezoidalIntegral(integralFunc, -alpha, alpha, N);
+	std::cout << "Estimated integral: " << res << std::endl;
+	std::cout << "Estimated error: " << estimateErrorTrap(integralFunc, -alpha, alpha, N) << std::endl;
+	std::cout << "Estimated order: " << calcOrder(integralFunc, -alpha, alpha, N) << std::endl;
+
+	res = ((Doub)1 / (Doub)1200) * res;
+	return res;
+}
+
+Doub cTrapNoCout(Doub inputY, int N) {
+	if (abs(inputY) > (Doub)0.2)
+		return (Doub)0.0;
 
 	Y = inputY;
 
 	Doub alpha = (Doub)0.05 * sqrt((Doub)1 - (pow(inputY, (Doub)2) / (Doub)0.04));
-	//std::cout << "Alpha: " << alpha << std::endl;
 
 	Doub res = trapezoidalIntegral(integralFunc, -alpha, alpha, N);
-	//std::cout << "Estimated integral: " << res << std::endl;
-	//std::cout << "Estimated error: " << estimateErrorTrap(integralFunc, -alpha, alpha, N) << std::endl;
 
 	res = ((Doub)1 / (Doub)1200) * res;
-	//std::cout << "res: " << res << std::endl;
-
 	return res;
 }
+
 
 Doub cRE(Doub inputY, int N) {
 	if (abs(inputY) > (Doub)0.2)
 		return (Doub)0.0;
 
-	//std::cout << "I made it past catch" << std::endl;
-
 	Y = inputY;
 
 	Doub alpha = (Doub)0.05 * sqrt((Doub)1 - (pow(inputY, (Doub)2) / (Doub)0.04));
-	std::cout << "Alpha: " << alpha << std::endl;
-
-	std::cout << "Estimated order: " << calcOrder(integralFunc, -alpha, alpha, N) << std::endl;
-
+	
 	Doub res = RE(integralFunc, -alpha, alpha, N);
 	std::cout << "Estimated integral: " << res << std::endl;
 	std::cout << "Estimated error: " << estimateErrorRE(integralFunc, -alpha, alpha, N) << std::endl;
 
 	res = ((Doub)1 / (Doub)1200) * res;
-	//std::cout << "res: " << res << std::endl;
-
 	return res;
 }
 
@@ -142,48 +145,25 @@ Doub minfunc(Doub Yi) {
 	return cTrap(Yi, 100) + cTrap(x - Yi, 100);
 }
 
-Doub minifunc(Doub ix) {
-	Doub Y = 0.1;
-	Golden g;
-
-	x = ix;
-
-	g.bracket(0.0, x, minfunc);
-	Doub min = g.minimize(minfunc);
-
-	std::cout << "d: " << x << std::endl;
-	std::cout << "cTrap1: " << cTrap(Y, 100) * 1000000 << std::endl;
-	std::cout << "cTrap2: " << cTrap(x - Y, 100) * 1000000 << std::endl;
-	std::cout << "min: " << min * 1000000 << std::endl;
-	//std::cout << "sum: " << (cTrap(Y, 100) + cTrap(x - Y, 100)) * 1000000 << std::endl;
-	std::cout << "sum: " << (min - (30 / 1000000)) * 1000000 << std::endl;
-	std::cout << std::endl;
-
-	return min - (30.0 / 1000000);
-}
-
-Doub minifunc2(Doub d) {
-
-	//std::cout << "cTrap1: " << cTrap(Y, 100) * 1000000 << std::endl;
-	//std::cout << "cTrap2: " << cTrap(d - Y, 100) * 1000000 << std::endl;
-
+Doub minifunc(Doub d) {
 
 	// Brute min
 	Doub acc = 0.00001;
-	Doub min = cTrap(0, 100) + cTrap(d - 0, 100);
+	Doub min = cTrapNoCout(0, 100) + cTrapNoCout(d - 0, 100);
 	Doub newMin;
 	Doub Yi = 0;
 	for (int i = 1; i < d / acc; i++) {
 		Yi = Yi + acc;
-		newMin = cTrap(Yi, 100) + cTrap(d - Yi, 100);
+		newMin = cTrapNoCout(Yi, 100) + cTrapNoCout(d - Yi, 100);
 		if (newMin < min)
 			min = newMin;
 	}
 
-	std::cout << "d: " << setprecision(15) << d << std::endl;
-	std::cout << "min: " << setprecision(15) << min * 1000000 << std::endl;
-	std::cout << "res: " << setprecision(15) << (min - (30.0 / 1000000)) * 1000000 << std::endl;
-	std::cout << std::endl;
+	// To see the iterative root find values, out comment below
+	//std::cout << "d: " << setprecision(15) << d << std::endl;
+	//std::cout << "min: " << setprecision(15) << min * 1000000 << std::endl;
+	//std::cout << "res: " << setprecision(15) << (min - (30.0 / 1000000)) * 1000000 << std::endl;
+	//std::cout << std::endl;
 
 	return min - (30.0 / 1000000);
 }
@@ -191,46 +171,29 @@ Doub minifunc2(Doub d) {
 
 int main() {
 
-	///*
-	int i = 8;
-
-	std::cout << "Calculation of c with trap, N = " << i << ": " << std::endl;
-	std::cout << "res with trap: " << cTrap(0.1, i) * 1000000 << " micrometer" << std::endl;
-	std::cout << std::endl;
-
-	std::cout << "Calculation of c with RE, N = " << i << ": " << std::endl;
-	std::cout << "res with trap: " << cRE(0.1, i) * 1000000 << " micrometer" << std::endl;
-	std::cout << std::endl;
-	//*/
-
-	/*
-
-	for (int i = 2; i < 12; i = i + 2) {
+	for (int i = 4; i < 33; i = i * 2) {
 		std::cout << "Calculation of c with trap, N = " << i << ": " << std::endl;
 		std::cout << "res with trap: " << cTrap(0.1, i) * 1000000 << " micrometer" << std::endl;
 		std::cout << std::endl;
 
 		std::cout << "Calculation of c with RE, N = " << i << ": " << std::endl;
-		std::cout << "res with trap: " << cRE(0.1, i) * 1000000 << " micrometer" << std::endl;
+		std::cout << "res with RE: " << cRE(0.1, i) * 1000000 << " micrometer" << std::endl;
 		std::cout << std::endl;
 	}
+	
+	
 
-	*/
-	/*
 	Doub Yi;
 	int N;
 	ofstream outputFile;
 	std::string data3 = "dataFor3.csv";
 	outputFile.open(data3);
 
-	//outputFile << "i" << "," << "Yi" << "," << "c(Yi)" << std::endl;
-
 	for (Doub i = 0; i < 50; i++) {
 		Yi = (i / 50.0)*0.2;
 		N = round((2 * calcAlpha(Yi)) / 0.001);
 		
-		//std::cout << "i: " << i << "	Yi: " << Yi << "	c(Yi): " << cTrap(Yi, N) * 1000000 << std::endl;
-		outputFile << i << "," << Yi << "," << cTrap(Yi, N) * 1000000 << std::endl;
+		outputFile << i << "," << Yi << "," << cTrapNoCout(Yi, N) * 1000000 << std::endl;
 	}
 
 	outputFile.close();
@@ -242,13 +205,14 @@ int main() {
 		Yi = (i / 50.0) * 0.2;
 		N = round((2 * calcAlpha(Yi)) / 0.001);
 		
-		outputFile << i << "," << Yi << "," << ((cTrap(Yi, 100) + cTrap(0.234389134310186 - Yi, 100)) * 1000000) << std::endl;
+		outputFile << i << "," << Yi << "," << ((cTrapNoCout(Yi, 100) + cTrapNoCout(0.234389134310186 - Yi, 100)) * 1000000) << std::endl;
 	}
 
 	outputFile.close();
 
-	std::cout << "Sex mig i røven: " << setprecision(15) << rtbis(minifunc2, 0.2, 0.3, pow(10, -10)) << std::endl;
-	*/
+	std::cout << "Calculating, this might take a minute..." << std::endl;
+	std::cout << "The minimum distance d is: " << setprecision(15) << rtbis(minifunc, 0.2, 0.3, pow(10, -10)) << std::endl;
+	
 
 
 	return 1;
